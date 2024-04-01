0f6843eaf2d2eb5d37f563d896b998f4e90e378a
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 18:22:52 -0500

    se crea en enum que representa los tipos de libros que hay

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Enums/BaseBookType.cs
@@ -0,0 +1,10 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.Entities;
+
+namespace Sofka.Piguinera.Cotizacion.Models.Enums
+{
+    public enum BaseBookType
+    {
+        Book,
+        Novel
+    }
+}
