784e564e4d56294ca2c8d3bae3c80603c82e982f
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 18:23:46 -0500

    se construyen los servicios para que funcione el programa

--- Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Program.cs
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Program.cs
@@ -1,8 +1,16 @@
+using FluentValidation;
+using Sofka.Piguinera.Cotizacion.Models.DTOS;
+using Sofka.Piguinera.Cotizacion.Models.Factories;
+using Sofka.Piguinera.Cotizacion.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IQuotesService, QuotesServiceImplementation>();
+builder.Services.AddSingleton<IBaseBookFactory, BaseBookFactory>();
+builder.Services.AddSingleton<IValidator<BaseBookDTO>, BaseBookDTO.BaseBookDTOValidator>();
 
 var app = builder.Build();
 
