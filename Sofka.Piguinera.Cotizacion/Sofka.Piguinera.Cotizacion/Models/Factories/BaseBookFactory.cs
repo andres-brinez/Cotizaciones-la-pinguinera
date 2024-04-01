6ea9a2188ac98600860e62d494de3a7d8f7c335d
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 18:23:23 -0500

    Se implementa el factory method para crear clases

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Factories/BaseBookFactory.cs
@@ -0,0 +1,39 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.DTOS;
+using Sofka.Piguinera.Cotizacion.Models.Entities;
+using Sofka.Piguinera.Cotizacion.Models.Enums;
+
+namespace Sofka.Piguinera.Cotizacion.Models.Factories
+{
+    public class BaseBookFactory : IBaseBookFactory
+    {
+
+        public BaseBook Create(BaseBookDTO payload)
+        {
+            var booksChildren = new Dictionary<BaseBookType, BaseBook>
+            {
+                {BaseBookType.Book, new Book(payload.Title,payload.OriginalPrice,payload.NameProvider,payload.Seniority)},
+                {BaseBookType.Novel, new Novel(payload.Title,payload.OriginalPrice,payload.NameProvider,payload.Seniority)},
+            };
+
+
+            Console.WriteLine("Creando un libro ");
+            Console.WriteLine("Tipo de libro: " + payload.Type);
+            Console.WriteLine("Nombre del proveedor: " + payload.NameProvider);
+            Console.WriteLine("Antiguedad del proveedor: " + payload.Seniority);
+
+            var Book = booksChildren[payload.Type];
+
+            if (Book == null)
+            {
+                throw new Exception("Tipo de libro no válido");
+            }
+
+            return Book;
+
+
+
+            
+        }
+          
+    }
+}
---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Factories/IBaseBookFactory.cs
@@ -0,0 +1,12 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.DTOS;
+using Sofka.Piguinera.Cotizacion.Models.Entities;
+
+namespace Sofka.Piguinera.Cotizacion.Models.Factories
+{
+    public interface IBaseBookFactory
+    {
+
+        BaseBook Create(BaseBookDTO payload);
+
+    }
+}
