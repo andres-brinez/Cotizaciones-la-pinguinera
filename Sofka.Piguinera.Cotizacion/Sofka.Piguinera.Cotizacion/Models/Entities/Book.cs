1b580c6baca9300a88cacdb405999480e8977202
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 18:21:59 -0500

    Se crea la clase que representa un  libro pero hereda de BaseBook

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Entities/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sofka.Piguinera.Cotizacion.Models.Entities
+{
+    public class Book : BaseBook
+    {
+        public Book(string title, int originalPrice, string nameProvider, int seniority) : base(title, originalPrice, nameProvider, seniority)
+        {
+        }
+
+        public override float CalculateTotalPrice()
+        {
+
+            const  Double CURRENT_INCREASE  = 0.33;
+
+            CalculateDiscount();
+
+            //El precio de los libros es aumentado 1/3 de su precio original,
+            decimal  totalPrice = (decimal)(OriginalPrice + (OriginalPrice* CURRENT_INCREASE));
+
+            CurrentPrice = (double)(totalPrice * (1 - Discount));
+
+            return (float)CurrentPrice ;
+        
+        }
+    }
+    
+    
+}
