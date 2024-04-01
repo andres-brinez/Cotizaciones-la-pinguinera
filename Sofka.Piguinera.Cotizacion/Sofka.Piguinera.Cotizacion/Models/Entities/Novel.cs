949289ba31e29ef822b336c083d0a64715820101
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 18:22:25 -0500

    Se crea la clase novela que implementa el método CalculateTotalPrice()

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Entities/Novel.cs
@@ -0,0 +1,25 @@
+﻿namespace Sofka.Piguinera.Cotizacion.Models.Entities
+{
+    public class Novel : BaseBook
+    {
+        public Novel(string title, int originalPrice, string nameProvider, int seniority) : base(title, originalPrice, nameProvider, seniority)
+        {
+        }
+
+        public override float CalculateTotalPrice()
+        {
+
+            const decimal CURRENT_INCREASE = 2;
+
+            CalculateDiscount();
+           
+            CurrentPrice = (double)(OriginalPrice * CURRENT_INCREASE * (1 - Discount));
+
+            return (float)CurrentPrice;
+        }
+
+
+    }
+
+
+}
