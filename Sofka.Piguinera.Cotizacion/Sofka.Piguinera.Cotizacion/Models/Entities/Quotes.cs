ab4d68e9923ff5acd499e0cc7ae1b0584e35ea08
Andres Briñez <brinezlopez08@gmail.com>
2024-04-01 20:30:51 -0500

    Se crea la clase que representa la cotización

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Entities/Quotes.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Sofka.Piguinera.Cotizacion.Models.Entities
+{
+    public class Quotes
+    {
+
+
+        // Tiene la información de los libros que se van a cotizar
+
+        public List<BaseBook> Books { get; set; }
+
+        //typePurchase
+        public  string TypePurchase { get; set; }
+
+
+        public float TotalPrices { get; set; }
+
+        //public const float DISCOUNT_Aplicados = 0.19f;
+
+        public Quotes()
+        {
+        }
+
+        public Quotes(List<BaseBook> books,float totalPrices,string typePurchase)
+        {
+            Books = books;
+            TotalPrices = totalPrices;
+            TypePurchase = typePurchase;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in Books)
+            {
+                sb.Append(book.ToString());
+            }
+
+            return $"Books and novels: \n" +
+                $"" +
+                $"{sb.ToString()}" +
+                $"- TotalPrices: {TotalPrices}\n"+
+                $"- TypePurchase: {TypePurchase}";
+
+
+
+        }
+
+
+
+    }
+}
