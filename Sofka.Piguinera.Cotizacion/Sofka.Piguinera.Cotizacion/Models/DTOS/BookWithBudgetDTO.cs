c3c20f5062d7808782e86b140461d7ca8487241c
Andres Briñez <brinezlopez08@gmail.com>
2024-04-02 13:19:39 -0500

    Se crea el DTO que recibe una lista de libros y el prestamo

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/DTOS/BookWithBudgetDTO.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace Sofka.Piguinera.Cotizacion.Models.DTOS
+{
+    public class BookWithBudgetDTO
+    {
+        public List<BaseBookDTO> Books { get; set; }
+        public decimal Budget { get; set; }
+
+        public BookWithBudgetDTO()
+        {
+        }
+
+        public BookWithBudgetDTO(List<BaseBookDTO> book, decimal budget)
+        {
+            Books = book;
+            Budget = budget;
+        }
+
+        public class BookWithBudgetDTOValidator : AbstractValidator<BookWithBudgetDTO>
+        {
+            public BookWithBudgetDTOValidator()
+            {
+                RuleFor(x => x.Books).NotNull().WithMessage("La lista de libros no puede ser nula");
+                RuleFor(x => x.Budget).GreaterThanOrEqualTo(0).WithMessage("El presupuesto no puede ser negativo");
+            }
+        }
+    }
+
+}
