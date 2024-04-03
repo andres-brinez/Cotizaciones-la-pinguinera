ffac5c10b33400acd8fe5cbf19e8ac0081389001
Andres Briñez <brinezlopez08@gmail.com>
2024-04-03 13:03:27 -0500

    Se le agrega al nombre de la clase "input" indicando que es un dto de entrada de datos

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/DTOS/OutputDTO/BaseBookOutputDTO.cs
@@ -0,0 +1,29 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.Enums;
+
+namespace Sofka.Piguinera.Cotizacion.Models.DTOS.OutputDTO
+{
+    public class BaseBookOutputDTO
+    {
+
+        private string Title { get; set; }
+        public BaseBookType Type { get; set; }
+        public float Price { get; set; }
+        public string Discount { get; set; }
+
+        public BaseBookOutputDTO()
+        {
+
+        }
+        public BaseBookOutputDTO(string title, BaseBookType type,  float price, float discount)
+        {
+
+            Title = title;
+            Type = type;
+            Price = (float)System.Math.Round(price, 2);
+
+            float discountPercentage = discount * 100;
+            Discount = discountPercentage.ToString() + "%";
+        }
+
+    }
+}
