d4148adee1c62e050e31ce800dde7d6b64115a18
Andres Briñez <brinezlopez08@gmail.com>
2024-04-03 13:35:01 -0500

    Se crea el dto que se encarga de mostrar la información de la posible compra de acuerdo a su presupuesto

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/DTOS/OutputDTO/BookWithBudgeOutputDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Sofka.Piguinera.Cotizacion.Models.DTOS.OutputDTO
+{
+    public class BookWithBudgeOutputDTO
+    {
+        public List <BaseBookOutputDTO> Books { get; set; }
+        public float Budget { get; set; }
+
+        public BookWithBudgeOutputDTO()
+        {
+            
+        }
+
+        public BookWithBudgeOutputDTO(List<BaseBookOutputDTO> books, float budget)
+        {
+            Books = books;
+            Budget = (float)System.Math.Round(budget, 2);
+        }
+
+
+
+    }
+}
