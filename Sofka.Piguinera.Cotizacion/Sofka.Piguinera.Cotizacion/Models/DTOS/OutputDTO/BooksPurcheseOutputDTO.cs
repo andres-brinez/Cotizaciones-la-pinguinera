cd196d010d3f275bcb1d3b0eda2ef6806ba18608
Andres Briñez <brinezlopez08@gmail.com>
2024-04-03 13:34:39 -0500

    Se crea el dto que se encarga de mostrar la información de la posible compra

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/DTOS/OutputDTO/BooksPurcheseOutputDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace Sofka.Piguinera.Cotizacion.Models.DTOS.OutputDTO
+{
+    public class BooksPurcheseOutputDTO
+    {
+
+        public List<BaseBookOutputDTO> Books { get; set; }
+        public float TotalPrice { get; set; }
+        public string TypePurchase { get; set; }
+
+        public BooksPurcheseOutputDTO()
+        {
+
+        }
+
+        public BooksPurcheseOutputDTO(List<BaseBookOutputDTO> books, float totalPrice, string typePurchase)
+        {
+            Books = books;
+            TotalPrice = (float)System.Math.Round(totalPrice, 2);
+            TypePurchase = typePurchase;
+        }
+
+    }
+}
