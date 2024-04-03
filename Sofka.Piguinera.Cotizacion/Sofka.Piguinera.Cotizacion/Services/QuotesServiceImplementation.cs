8c4e2f0aaf41f7172c5bb03b43cfb5c7cf8ab379
Andres Briñez <brinezlopez08@gmail.com>
2024-04-03 00:49:50 -0500

    Se refactoriza el método showInformation con el método BooksBudget

--- Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Services/QuotesServiceImplementation.cs
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Services/QuotesServiceImplementation.cs
@@ -2,6 +2,7 @@
 using Sofka.Piguinera.Cotizacion.Models.Entities;
 using Sofka.Piguinera.Cotizacion.Models.Enums;
 using Sofka.Piguinera.Cotizacion.Models.Factories;
+using Sofka.Piguinera.Cotizacion.Utils;
 using System.Text;
 using static System.Reflection.Metadata.BlobBuilder;
 
@@ -17,10 +18,7 @@ namespace Sofka.Piguinera.Cotizacion.Services
         {
             _baseBookFactory = baseBookFactory;
         }
-
         
-
-        // Calculate value to pay
         public string TotalPricePurchese(BaseBookDTO payload)
         {
             var book= _baseBookFactory.Create(payload);
@@ -32,8 +30,6 @@ namespace Sofka.Piguinera.Cotizacion.Services
             return book.ToString();
         }
 
-
-        // Calculate value to pay
         public String TotalPricePurcheses(List<BaseBookDTO> payload)
         {
 
@@ -54,78 +50,57 @@ namespace Sofka.Piguinera.Cotizacion.Services
 
         public string BooksBudget(BookWithBudgetDTO payload)
         {
-
             var books = payload.Books.Select(item => _baseBookFactory.Create(item)).ToList();
-
             BookPricingService.CalculatePurcheseValue(books);
-
-
             books = books.OrderByDescending(item => item.Discount).ToList();
 
             double totalBudgetAvailable = (double)payload.Budget;
+            List<BaseBook> booksAvailable = SelectBooksWithinBudget(books, ref totalBudgetAvailable);
 
-            List<BaseBook> booksAvailable = new List<BaseBook>();
-
-            bool hasBook = false;
-            bool hasNovel = false;
+            string message = "No se puede comprar ningun libro con el presupuesto actual";
 
 
-            List<BaseBook> booksAvailableCopy = new List<BaseBook>();
+            return booksAvailable.Count == 0 ? message : ShowInformation.Show(booksAvailable, totalBudgetAvailable);
 
-            foreach (var book in books)
+        }
+
+        private List<BaseBook> SelectBooksWithinBudget(List<BaseBook> books, ref double totalBudgetAvailable)
             {
-                bool isAvailableBudget = totalBudgetAvailable > book.CurrentPrice;
-                bool shouldAddBook = isAvailableBudget && (
-                    (book.Type == BaseBookType.Novel && !hasNovel) ||
-                    (book.Type == BaseBookType.Book && !hasBook) ||
-                    (hasBook && hasNovel));
+                List<BaseBook> booksAvailable = new List<BaseBook>();
+                bool hasBook = false;
+                bool hasNovel = false;
 
-
-                if (shouldAddBook)
+                foreach (var book in books)
                 {
-                    booksAvailable.Add(book);
-                    totalBudgetAvailable -= book.CurrentPrice;
+                    bool isAvailableBudget = totalBudgetAvailable > book.CurrentPrice;
+                    bool shouldAddBook = isAvailableBudget && (
+                        (book.Type == BaseBookType.Novel && !hasNovel) ||
+                        (book.Type == BaseBookType.Book && !hasBook) ||
+                        (hasBook && hasNovel));
 
+                    if (shouldAddBook)
+                    {
+                        booksAvailable.Add(book);
+                        totalBudgetAvailable -= book.CurrentPrice;
 
-                    if (book.Type == BaseBookType.Novel)
-                    {
-                        hasNovel = true;
-                    }
-                    else if (book.Type == BaseBookType.Book)
-                    {
-                        hasBook = true;
+                        if (book.Type == BaseBookType.Novel)
+                        {
+                            hasNovel = true;
+                        }
+                        else if (book.Type == BaseBookType.Book)
+                        {
+                            hasBook = true;
+                        }
                     }
                 }
-                
+
+                return booksAvailable;
             }
 
 
-            string mensaje = "No se puede comprar ningun libro con el presupuesto actual";
-
-            return booksAvailable.Count == 0 ? mensaje : showInformation(booksAvailable, totalBudgetAvailable);
-
-        }
 
 
-        public string showInformation(List<BaseBook> books, double budget)
-        {
-            // retorna el presupuesto  con los libros que se pueden comprar
-
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var book in books)
-            {
-                sb.Append(book.ToString());
-            }
-
-            return $"Books and novels: \n" +
-                $"" +
-                $"{sb.ToString()}\n" +
-              //  $"- Total Prices: {TotalPrices}\n" +
-                $"- Budget: {budget}";
-
-
-        }
+       
 
     }
 
---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Utils/ShowInformation.cs
@@ -0,0 +1,29 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.Entities;
+using System.Text;
+
+namespace Sofka.Piguinera.Cotizacion.Utils
+{
+    public class ShowInformation
+    {
+
+        public static string Show(List<BaseBook> books, double budget)
+        {
+            // retorna el presupuesto  con los libros que se pueden comprar
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.Append(book.ToString());
+            }
+
+            return $"Books and novels: \n" +
+                $"" +
+                $"{sb.ToString()}\n" +
+                //  $"- Total Prices: {TotalPrices}\n" +
+                $"- Budget: {budget}";
+
+
+        }
+    }
+}
