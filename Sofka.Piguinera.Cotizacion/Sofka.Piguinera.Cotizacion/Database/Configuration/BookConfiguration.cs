9e07655b24b69072a2271b873c3469a2bf70a3e3
Andres Briñez <brinezlopez08@gmail.com>
2024-04-04 13:35:25 -0500

    Configuración para la base de datos

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Database/Configuration/BookConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sofka.Piguinera.Cotizacion.Models.Persistence;
+
+namespace Sofka.Piguinera.Cotizacion.Database.Configuration
+{
+    public class BookConfiguration
+    {
+        public  BookConfiguration(EntityTypeBuilder<BookPersistence> entityBuilder)
+        {
+            entityBuilder.HasKey(entity => entity.Id);
+            entityBuilder.Property(entity => entity.Title).IsRequired();
+            entityBuilder.Property(entity => entity.NameProvider).IsRequired();
+            entityBuilder.Property(entity => entity.Seniority).IsRequired();
+            entityBuilder.Property(entity => entity.OriginalPrice).IsRequired();
+            entityBuilder.Property(entity => entity.Quantity).IsRequired();
+            entityBuilder.Property(entity => entity.Type).IsRequired();
+            entityBuilder.Property(entity => entity.UnitPrice).IsRequired();
+            entityBuilder.Property(entity => entity.Discount).IsRequired();
+        }
+    }
+}
