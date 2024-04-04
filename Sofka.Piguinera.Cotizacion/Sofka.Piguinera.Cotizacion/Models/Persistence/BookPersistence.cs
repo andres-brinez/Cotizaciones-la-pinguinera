8cc5610d24934f3ab6b6cedb2c7b29ca1f37110b
Andres Briñez <brinezlopez08@gmail.com>
2024-04-04 13:37:08 -0500

    Se crea el modelo de persistencia

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Models/Persistence/BookPersistence.cs
@@ -0,0 +1,16 @@
+﻿namespace Sofka.Piguinera.Cotizacion.Models.Persistence
+{
+    public class BookPersistence
+    {
+        public string Id { get; set; }
+        public string? Title { get; set; }
+        public string? NameProvider { get; set; }
+        public int? Seniority { get; set; }
+        public int? OriginalPrice { get; set; }
+        public int? Quantity { get; set; }
+        public byte Type { get; set; }
+        public double UnitPrice { get; set; }
+        public float Discount { get; set; }
+
+    }
+}
