aacdf63b680ee141162172ae4581a1b934af2f98
Andres Briñez <brinezlopez08@gmail.com>
2024-04-05 02:39:27 -0500

    Se cambia el nombre de la clase

---
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Services/Interface/IQuotesService.cs
@@ -0,0 +1,17 @@
+﻿using Sofka.Piguinera.Cotizacion.Models.DTOS.Input;
+using Sofka.Piguinera.Cotizacion.Models.DTOS.InputDTO;
+using Sofka.Piguinera.Cotizacion.Models.DTOS.OutputDTO;
+using Sofka.Piguinera.Cotizacion.Models.Entities;
+
+namespace Sofka.Piguinera.Cotizacion.Services.Interface
+{
+    public interface IQuotesService
+    {
+
+
+        BooksPurcheseOutputDTO TotalPriceQuotes(List<InformationInputDto> payload);
+
+        BookWithBudgeOutputDTO BooksBudget(BookWithBudgeInputDTO payload);
+
+    }
+}
