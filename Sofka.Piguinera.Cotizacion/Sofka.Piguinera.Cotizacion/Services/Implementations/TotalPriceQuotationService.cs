6bc3ace626d1e43d2866163a2d2b36e18267b776
Andres Briñez <brinezlopez08@gmail.com>
2024-04-17 23:14:49 -0500

    Se obtiene el userProvider de la base de datos y se pasa a la entidad

--- Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Services/Implementations/TotalPriceQuotationService.cs
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Services/Implementations/TotalPriceQuotationService.cs
@@ -3,6 +3,7 @@ using Sofka.Piguinera.Cotizacion.Database.Configuration.Interfaces;
 using Sofka.Piguinera.Cotizacion.DesignPattern.Factories;
 using Sofka.Piguinera.Cotizacion.Models.DTOS.InputDTO;
 using Sofka.Piguinera.Cotizacion.Models.DTOS.OutputDTO;
+using Sofka.Piguinera.Cotizacion.Models.Entities;
 using Sofka.Piguinera.Cotizacion.Models.Enums;
 using Sofka.Piguinera.Cotizacion.Models.Persistence;
 using Sofka.Piguinera.Cotizacion.Services.Interface;
@@ -23,20 +24,29 @@ namespace Sofka.Piguinera.Cotizacion.Services.Implementations
 
         public async Task<BaseBookOutputDTO> CalculateTotalPriceQuotation(BaseBookInputDTO payload)
         {
-            var bookEntity = _baseBookFactory.CreateBookEntity(payload.Type, payload.Id, payload.Title, payload.OriginalPrice, payload.NameProvider, payload.Seniority, payload.Quantity);
+            UserPersistence userProvider = _databaseService.GetUser(payload.EmailProvider);
 
-            // Se calcula el precio  del libro con los descuentos aplicados 
-            bookEntity.CalculateTotalPrice();
-
-            BaseBookOutputDTO baseBookOutputDTO = new BaseBookOutputDTO(bookEntity.Title, bookEntity.Type, bookEntity.CurrentPrice, bookEntity.Discount, bookEntity.Cuantity);
-            var bookPersistence = _baseBookFactory.BookEntityToPersistence(bookEntity);
-
-            if (!await _databaseService.AddBookAsync(bookPersistence))
+            if (userProvider != null)
             {
-                return null;
+                BaseBookEntity bookEntity = _baseBookFactory.CreateBookEntity(payload.Type, payload.Id, payload.Title, payload.OriginalPrice, payload.EmailProvider, (int)userProvider.Seniority, payload.Quantity);
+
+                // Se calcula el precio  del libro con los descuentos aplicados 
+                bookEntity.CalculateTotalPrice();
+
+                BaseBookOutputDTO baseBookOutputDTO = new BaseBookOutputDTO(bookEntity.Title, bookEntity.Type, bookEntity.CurrentPrice, bookEntity.Discount, bookEntity.Cuantity);
+                var bookPersistence = _baseBookFactory.BookEntityToPersistence(bookEntity);
+
+                if (!await _databaseService.AddBookAsync(bookPersistence))
+                {
+                    return null;
+                }
+
+                return baseBookOutputDTO;
+
             }
 
-            return baseBookOutputDTO;
+            return null;
+            
         }
     }
 }
