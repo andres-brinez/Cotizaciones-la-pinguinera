e07673e36e9ba1af20db13bfff75f8076f7f3aee
Andres Briñez <brinezlopez08@gmail.com>
2024-04-17 22:16:38 -0500

    Se agregan las anotaciones de Swagger para la documentación del controlador

--- Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Controllers/UserController.cs
+++ Sofka.Piguinera.Cotizacion/Sofka.Piguinera.Cotizacion/Controllers/UserController.cs
@@ -2,11 +2,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Sofka.Piguinera.Cotizacion.Models.DTOS.Input;
 using Sofka.Piguinera.Cotizacion.Services.Interface;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel;
 
 namespace Sofka.Piguinera.Cotizacion.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Description("Controller for handling user operations.")]
+
     public class UserController : ControllerBase
     {
         private readonly IAuthenticationService _authenticationService;
@@ -21,6 +25,12 @@ namespace Sofka.Piguinera.Cotizacion.Controllers
         }
 
         [HttpPost("Register")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [SwaggerOperation(Summary = "Register a new user", Description = "This method takes a RegisterUserInputDTO object as input, validates it, and then registers the user.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Returns a string indicating that the user was registered successfully.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "If the item is null or validation fails.")]
         public async Task<IActionResult> RegisterUser([FromBody] RegisterUserInputDTO user)
         {
 
@@ -54,6 +64,12 @@ namespace Sofka.Piguinera.Cotizacion.Controllers
         }
 
         [HttpPost("Login")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [SwaggerOperation(Summary = "Login a user", Description = "This method takes a LoginUserInputDTO object as input, validates it, and then logs in the user.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Returns a string indicating that the user was logged in successfully.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "If the item is null or validation fails.")]
         public async Task<IActionResult> LoginUser([FromBody] LoginUserInputDTO user)
         {
             var validationResult = await _loginValidator.ValidateAsync(user);
